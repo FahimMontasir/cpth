d39018016b4e202b801f28a2effcf83bd7202a73
FahimMontasir <fahimmontasirshakil@gmail.com>
2021-07-26 12:05:55 +0600

    setup project

---
+++ src/screens/BlogScreen/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const BlogScreen = () => {
+  return (
+    <View>
+      <Text>BlogScreen</Text>
+    </View>
+  );
+};
+export default BlogScreen;
---
+++ src/screens/CourseScreen/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const CourseScreen = () => {
+  return (
+    <View>
+      <Text>CourseScreen</Text>
+    </View>
+  );
+};
+export default CourseScreen;
